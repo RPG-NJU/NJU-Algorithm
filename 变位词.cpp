2a3f6cbf50d1027a2916eb28706425fdf74e1bfa
RPG <41836383+HELLORPG@users.noreply.github.com>
2019-04-10 22:10:45 +0800

    Contest3 代码文件上传

---
+++ 变位词.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <complex>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ostream;
+
+using std::string;
+using std::vector;
+
+using std::sort;
+using std::min;
+
+
+// #define GRPDEBUG
+
+// 给出一些自测试的样例
+/*
+
+9
+a ew vc tea oe eat zoo el le 
+
+20 
+tea for aet slu uls int shi ni ish hsi what for if it fi a his from a you 
+
+*/
+
+struct Word
+{
+	string original_word;
+	string tag;
+	int index;
+};
+
+ostream &operator<<(ostream &os, const Word &output);
+
+void sort_by_original(vector<Word> &data);
+
+bool compare_word(const Word &x, const Word &y);
+
+void build_tag(vector<Word> &data);
+
+void sort_by_tag(vector<Word> &data);
+
+bool compare_tag(const Word &x, const Word &y);
+
+void find_anagram(const vector<Word> &data, vector<string> &result);
+
+int main()
+{
+	int n(0);
+	vector<Word> data;
+	vector<string> result;
+	string input;
+
+	cin >> n;
+	for (int i(0); i < n; ++i)
+	{
+		cin >> input;
+		Word x{ input, input, -1 };
+		// data.push_back({ input, input, -1 });
+		data.push_back(x);
+	}
+	// 完成了所有的输入
+
+#ifdef GRPDEBUG
+	cout << "[ALL INPUT SHOW]    ";
+	for (const auto &x : data)
+		cout << x << " ";
+	cout << endl << endl;
+#endif
+
+	sort_by_original(data);
+
+#ifdef GRPDEBUG
+	cout << "[AFTER SORT BY WORD]    ";
+	for (const auto &x : data)
+		cout << x << " ";
+	cout << endl << endl;
+#endif
+
+	build_tag(data);
+
+#ifdef GRPDEBUG
+	cout << "[AFTER BUILD TAG]    ";
+	for (const auto &x : data)
+		cout << x << " ";
+	cout << endl << endl;
+#endif
+
+	sort_by_tag(data);
+
+#ifdef GRPDEBUG
+	cout << "[AFTER SORT BY TAG]    ";
+	for (const auto &x : data)
+		cout << x << " ";
+	cout << endl << endl;
+#endif
+
+	find_anagram(data, result);
+
+	cout << "wo yi yue du guan yu chao xi de shuo ming" << endl;
+	// 按照助教的要求，输出“我已阅读关于抄袭的说明”
+
+
+
+	const int length(result.size());
+	cout << length << endl;
+	// 输出易位词的对数
+
+	if (length == 0)
+	{
+		
+	}
+	else
+	{
+		cout << result[0];
+		for (int i(1); i < length; ++i)
+			cout << endl << result[i];
+	}
+
+
+	return 0;
+}
+
+ostream& operator<<(ostream& os, const Word& output)
+{
+	os << "{" << output.original_word << " " << output.tag << " " << output.index << "}";
+	return os;
+}
+
+
+void sort_by_original(vector<Word>& data)
+{
+	sort(data.begin(), data.end(), compare_word);
+
+	// 需要更新index供后续进行操作
+	const int length = data.size();
+	for (int i(0); i < length; ++i)
+		data[i].index = i;
+
+	return;
+}
+
+bool compare_word(const Word& x, const Word& y)
+{
+	return x.original_word < y.original_word;
+}
+
+void build_tag(vector<Word>& data)
+{
+	//for (auto &x : data)
+	//{
+	//	sort(x.tag.begin(), x.tag.end());
+	//}
+	const int length(data.size());
+	for (int i(0); i < length; ++i)
+		sort(data[i].tag.begin(), data[i].tag.end());
+	return;
+}
+
+void sort_by_tag(vector<Word>& data)
+{
+	sort(data.begin(), data.end(), compare_tag);
+
+	return;
+}
+
+bool compare_tag(const Word& x, const Word& y)
+{
+	return x.tag < y.tag;
+}
+
+void find_anagram(const vector<Word>& data, vector<string>& result)
+{
+	const int length(data.size());
+
+	if (length == 1 || length == 0)
+		return;
+	// 个人认为不会出现的特殊情况，但是还是进行了讨论
+
+	else
+	{
+		int min_index(INT32_MAX);
+		int real_index(-1);
+		for (int i(1), j(0); i < length; ++i, ++j)
+		{
+			if (data[i].tag == data[j].tag)// 此时就存在了易位词
+			{
+				if (min_index == INT32_MAX)
+				{
+					min_index = min(data[i].index, data[j].index);
+					if (data[i].index > data[j].index)
+						real_index = j;
+					else
+						real_index = i;
+				}
+				else
+				{
+					const int old_min = min_index;
+					min_index = min(data[i].index, min_index);
+					if (data[i].index < old_min)
+						real_index = i;
+				}
+#ifdef GRPDEBUG
+				cout << "(" << data[i].tag << ") in " << min_index << endl;
+				cout << (i == length - 1) << endl;
+#endif
+				if (i == length - 1)
+					goto special;
+			}
+
+			else
+			{
+				special:
+				if (min_index == INT32_MAX)
+					continue;
+				else
+				{
+					result.push_back(data[real_index].original_word);
+#ifdef GRPDEBUG
+					cout << "[" << real_index << "] is " << data[real_index].original_word << endl;
+#endif
+					min_index = INT32_MAX;
+				}
+			}
+		}
+	}
+
+	sort(result.begin(), result.end());
+
+	return;
+}
---
+++ 所有逆序对.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::sort;
+
+
+// #define DEBUG_SHOW
+
+
+// 长度的类型最好使用long long，因为数据可能会很大
+
+long long ReversePair(vector<string> &list, int left, int right);
+
+long long Merge(vector<string> &list, int left, int mid, int right);
+// 需要将跨越中线的两个部分中的逆序对数量计算出来
+// 同时重新归并成为升序排列的序列
+
+
+int main()
+{
+	/*string a("apple"), b("banana");
+	cout << (a < b) << endl;*/
+	//对于string类型可以直接使用大于小于进行字典序判定
+
+	/*
+	 * 采取归并的方式进行求解
+	 * 将一个序列从正中间分开，分为两个子问题求解
+	 * 在两个序列中分别求出逆序对的个数，再将两个序列中跨序列的逆序对找出，进行合并求解
+	 * 之后将两个序列合并排序，得到一个升序序列，方便下一次的求解
+	 */
+
+	
+
+	int n(0);
+	string input;
+	vector<string> data;
+
+	cin >> n;
+	// 输入个数
+	for (int i(0); i < n; ++i)
+	{
+		cin >> input;
+		data.push_back(input);
+	}
+	// 完成了输入
+	
+#ifdef DEBUG_SHOW
+	cout << "-----> INPUT ALL SHOW:" << endl;
+	for (auto word : data)
+		cout << word << " ";
+	cout << endl;
+#endif
+
+	cout << "wo yi yue du guan yu chao xi de shuo ming" << endl;
+	// 输出老师要求的特殊字符，我已阅读关于抄袭的说明
+
+	cout << ReversePair(data, 0, n - 1) << endl;
+	return 0;
+}
+
+long long ReversePair(vector<string>& list, int left, int right)
+{
+	if (left == right)
+		return 0; // base-case
+
+	// recursion-case
+	long long mid = (left + right) / 2;
+
+	long long left_recursion = ReversePair(list, left, mid);
+	long long right_recursion = ReversePair(list, mid + 1, right);
+	long long merge_result = Merge(list, left, mid, right);
+
+	return left_recursion + merge_result + right_recursion;
+}
+
+long long Merge(vector<string>& list, int left, int mid, int right)
+{
+	// 用于将两个部分，合并计数，并且排序
+	long long reverse_pair_count(0); // 用于计数
+	long long i(left), j(mid + 1); // 作为遍历的两个指针
+
+	for (; i <= mid && j <= right; )
+	{
+		if (list[i] > list[j])
+		{
+			reverse_pair_count += mid - i + 1;
+			// 每次逆序元素，逆序对数量增加，m-i+1即为该元素到左序列尾的元素数量
+			++j;
+		}
+
+		else
+		{
+			++i;
+		}
+	}
+
+	sort(&list[left], &list[right] + 1);
+
+	return reverse_pair_count;
+}
